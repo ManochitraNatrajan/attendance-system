14deb58c098813fa2870e971116700e6983cbb2b
ManochitraNatrajan <manochitra.mages@gmail.com>
2026-06-04 01:09:05 +0530

    chore: remove test files and sync db.json with MongoDB data

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.li2hd4pfhik"
+    "revision": "0.593kfpocivg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
